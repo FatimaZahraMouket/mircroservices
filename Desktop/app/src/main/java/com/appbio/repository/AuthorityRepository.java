c27fb402a61f97a434c18a032260bb8e32d55ff9
Fatima Zahra Mouket <fatimazahra20mouket@gmail.com>
2023-10-04 12:31:21 +0100

    Initial version of appbiomedicale generated by generator-jhipster@7.9.3

---
+++ Desktop/app/src/main/java/com/appbio/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.appbio.repository;
+
+import com.appbio.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
